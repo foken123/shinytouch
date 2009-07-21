4d49c7ab8ae1bc6fb88bdd48cd59c2044ad3581a
antimatter15 <antimatter15@56b13c6e-67d3-11de-a84c-2f1c77ec4fbd>
2009-07-21 15:20:42 +0000

    new hue tri-diff

--- trunk/reflection.py
+++ trunk/reflection.py
@@ -62,7 +62,7 @@ def colorTargetMatch(c):
         return True
   return False
 
-def colorTest(x, y, dolog = False):
+def colorTestLineWidth(x, y, dolog = False):
   global imv, testmode, buildrange, draw, pix
   if buildrange == True:
     return False
@@ -187,7 +187,55 @@ def colorTest3(x, y, dolog = False):
     return True
   
   return False
-    
+
+
+
+def colorTest(x, y, dolog = False):
+  global imv, testmode, buildrange, draw, pix
+  if buildrange == True:
+    return False
+  c = pix[x+10,y] #reflection
+  d = pix[x+10,y-15] #background
+  t = pix[x-5,y] #color of the finger
+  
+  ihIM = fdivide(hsv(c)[0]-hsv(d)[0],hsv(t)[0]-hsv(d)[0])
+  
+  pix[x+10,y] = (255,255,255,255)
+  pix[x+10,y-15] = (255,0,255,255)
+  pix[x-5,y] = (0,0,255,255)
+  
+  
+  #draw.line(((x,y),(x+40,y)), fill=(ra,ga,ba))
+  
+  if dolog == True:
+    print "Finger ",t
+    print "Reflection ",c
+    print "Background ",d
+    print "Ideal Hue IM",ihIM
+  #if r > -55 and r < -20:
+  #  if g > -20 and g < 10:
+  #    if b > -25 and b < 5:
+  #      return True
+  
+  
+  draw.rectangle(((0,0),(40,40)), fill=t)
+  draw.rectangle(((40,0),(80,40)), fill=c)
+  draw.rectangle(((80,0),(120,40)), fill=d)
+  
+  
+  draw.text((0,20), str(int(ihIM*10000)/100), fill=(0,0,0))
+  
+  draw.line(((100, 30), (100+int(ihIM*10000)/100, 30)), fill=(0,255,0), width=10)
+  
+  pix[x+10,y] = (255,255,255,255)
+  pix[x+10,y-15] = (255,0,255,255)
+  pix[x-5,y] = (0,0,255,255)
+  
+  if ihIM > 0.1:
+    return True
+  
+  return False
+
 #this project is a failure
 
 def colorDiffGrade(c,d):
@@ -196,9 +244,12 @@ def colorDiffGrade(c,d):
   b = c[2]-d[2]
   return abs(r) + abs(g) + abs(b)
 
+def hsv(c):
+  return colorsys.rgb_to_hsv(c[0]/255.0, c[1]/255.0, c[2]/255.0)
+
 def hueDiffGrade(c, d):
-  chsv = colorsys.rgb_to_hsv(c[0]/255.0, c[1]/255.0, c[2]/255.0)
-  dhsv = colorsys.rgb_to_hsv(d[0]/255.0, d[1]/255.0, d[2]/255.0)
+  chsv = hsv(c)
+  dhsv = hsv(d)
   return dhsv[0]-chsv[0]
   
 def colorDiffAverage(c,d):
