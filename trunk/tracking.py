25221bdb126b56b7222dd6f29a40ce87544fc590
antimatter15 <antimatter15@56b13c6e-67d3-11de-a84c-2f1c77ec4fbd>
2009-07-26 14:48:50 +0000

    working autocalibrate

--- trunk/tracking.py
+++ trunk/tracking.py
@@ -7,7 +7,7 @@
 
 oldpix = 0
 oldim = 0
-autocal = 1
+autocal = 0
 stateframecount = 0
 switchcount = 0
 scantimes = 0
@@ -15,93 +15,8 @@ scantimes = 0
 diffmap = Image.new("RGB",(640,480))
 diffpix = diffmap.load()
 
-def colorDiffGrade(c,d):
-  r = c[0]-d[0]
-  g = c[1]-d[1]
-  b = c[2]-d[2]
-  return abs(r) + abs(g) + abs(b)
+execfile("autocalibrate.py")
 
-
-def img_diff(w, h):
-  global diffmap, pix, oldpix, diffpix, scantimes
-  backup = diffmap
-  backuppix = diffpix
-  count = 0
-  for x in range(0, w):
-    for y in range(0, h):
-      if colorDiffGrade(pix[x,y], oldpix[x,y]) > 20:
-        count += 1
-        diffpix[x,y] = (diffpix[x,y][0]+1,diffpix[x,y][1]+1,diffpix[x,y][2]+1)
-  ratio = float(count)/float(w*h)
-  print "R:",ratio
-  if ratio < 0.5:
-    scantimes += 1
-    return diffmap
-  else:
-    diffmap = backup
-    diffpix = backuppix
-    return False
-
-
-def average_img(w, h):
-  global diffpix
-  mt = 50
-  for x in range(0, w):
-    for y in range(0, h):
-      diffpix[x,y] = (mt*diffpix[x,y][0]/scantimes,mt*diffpix[x,y][1]/scantimes,mt*diffpix[x,y][2]/scantimes)
-      
-def get_points(w, h):
-  global diffpix, diffmap
-  average_img(w,h)
-  diffdraw = ImageDraw.Draw(diffmap)
-  lvc = 0
-  tfvw = 0
-  fvw = 0
-  lvw = 0
-  yarr = []
-  for x in range(0,w):
-    vc = 0
-    pvc = 0
-    pve = 0
-    for y in range(0, h):
-      if diffpix[x,y][0] > 60:
-        vc += 1
-      else:
-        if vc > pvc:
-          pvc = vc
-          pve = y
-        vc = 0
-    yarr.append((pve-pvc,pve))
-    
-    #diffdraw.line(((x,0),(x,pve)),fill=(0,255,255))
-    #diffdraw.line(((x,0),(x,pvc)),fill=(255,255,255))
-    diffpix[x,pvc] = (255,255,255)
-    diffpix[x,pve] = (0,255,255)
-    diffpix[x,pve-pvc] = (0,255,0)
-    if pvc > lvc:
-      lvc = pvc
-      if tfvw == 0 and pvc > 30:
-        tfvw = x
-    elif pvc > 30:
-      lvw = x
-      fvw = tfvw
-    
-  diffdraw.line(((lvw,0),(lvw,h)),fill=(0,0,255))
-  diffdraw.line(((fvw,0),(fvw,h)),fill=(255,0,0))
-  
-  diffdraw.line(((0,30),(w,30)),fill=(255,0,0))
-  #fvw is first (left)
-  #lvw is last (right)
-  #[0] is start x (top)
-  #[1] is end x (bottom)
-  diffdraw.line(((fvw,yarr[fvw][0]),(fvw,yarr[fvw][0])),fill=(0,255,255))
-  diffdraw.line(((lvw,yarr[lvw][1]),(lvw,yarr[lvw][1])),fill=(0,255,255))
-  
-  #diffdraw.line(((0,pvc),(w,pvc)),fill=(0,0,255))
-  #diffdraw.line(((0,pvc),(w,pvc)),fill=(0,0,255))
-  #diffdraw.line(((0,pvc),(w,pvc)),fill=(0,0,255))
-  
-  
 def get_image(dolog = False, getpix = False):
   global im, pix, draw, imsrc, autocal, stateframecount,switchcount, oldpix, oldim
   if imsrc == "cam":
@@ -119,10 +34,17 @@ def get_image(dolog = False, getpix = False):
     get_points(640, 480)
     #autocal = 0
     #switchcount = 0
-    #saveconfig()
+    saveconfig()
     switchcount += 1
   if switchcount > 7:
-    return diffmap
+    if switchcount < 20:
+      switchcount += 1
+      return diffmap
+    else:
+      autocal = 0
+      switchcount = 0
+      scantimes = 0
+      
   if autocal == 1:
     stateframecount += 1
     if stateframecount > 3:
