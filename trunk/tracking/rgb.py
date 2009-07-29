7776e5d0aedb4853085d57cecf74f614f519180d
antimatter15 <antimatter15@56b13c6e-67d3-11de-a84c-2f1c77ec4fbd>
2009-07-29 19:07:12 +0000

    renaming and removing

---
+++ trunk/tracking/rgb.py
@@ -0,0 +1,8 @@
+pygame.display.set_mode(resolution=(0,0), flags=0, depth=0): return Surface
+
+def colorTargetMatch(c):
+if c[0] < rmax and c[0] > rmin: #red
+  if c[1] < gmax and c[1] > gmin: #green
+    if c[2] < bmax and c[2] > bmin: #blue
+      return True
+return False
