5b5aeee1985448fef30f75cebf68a065981a0753
antimatter15 <antimatter15@56b13c6e-67d3-11de-a84c-2f1c77ec4fbd>
2009-07-29 19:44:40 +0000

    i forgot

--- tracking/rgb.py
+++ tracking/rgb.py
@@ -1,4 +1,4 @@
-pygame.display.set_mode(resolution=(0,0), flags=0, depth=0): return Surface
+#pygame.display.set_mode(resolution=(0,0), flags=0, depth=0): return Surface
 
 def colorTargetMatch(c):
 if c[0] < rmax and c[0] > rmin: #red
